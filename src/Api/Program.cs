86dc3f1083004d4f82b024c1b6db3e21aea59626
Alex Nikolaev <nlxn@yandex.ru>
2024-10-28 22:35:44 +0500

    Create minimal api app with authentication

---
+++ src/Api/Program.cs
@@ -0,0 +1,120 @@
+using Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Service;
+using Service.UserGroup;
+using System.Text;
+
+namespace Api
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+
+            var builder = WebApplication.CreateBuilder(args);
+            var connectionStr = builder.Configuration.GetConnectionString("DefaultConnection");
+            var appSettings = builder.Configuration.GetSection("TokenSettings").Get<TokenSettings>() ?? default!;
+            builder.Services.AddSingleton(appSettings);
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                            options.UseInMemoryDatabase("AppDb"));
+
+            builder.Services.AddIdentityCore<ApplicationUser>()
+                .AddRoles<IdentityRole>()
+                .AddSignInManager()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddTokenProvider<DataProtectorTokenProvider<ApplicationUser>>("REFRESHTOKENPROVIDER");
+
+            builder.Services.Configure<DataProtectionTokenProviderOptions>(options =>
+            {
+                options.TokenLifespan = TimeSpan.FromSeconds(appSettings.RefreshTokenExpireSeconds);
+            });
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+                {
+                    options.RequireHttpsMetadata = false;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        RequireExpirationTime = true,
+                        ValidIssuer = appSettings.Issuer,
+                        ValidAudience = appSettings.Audience,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(appSettings.SecretKey)),
+                        ClockSkew = TimeSpan.FromSeconds(0)
+                    };
+                });
+
+            builder.Services.AddScoped<ApplicationDbContextInitialiser>();
+            builder.Services.AddTransient<UserService>();
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("webAppRequests", builder =>
+                {
+                    builder.AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .WithOrigins(appSettings.Audience)
+                    .AllowCredentials();
+                });
+            });
+
+            builder.Services.AddSwaggerGen(config =>
+            {
+                config.SwaggerDoc("v1", new OpenApiInfo() { Title = "App Api", Version = "v1" });
+                config.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "bearer"
+                });
+                config.AddSecurityRequirement(
+                    new OpenApiSecurityRequirement{
+                        {
+                            new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type=ReferenceType.SecurityScheme,
+                                    Id="Bearer"
+                                }
+                            },
+                            Array.Empty<string>()
+                        }
+                    });
+            });
+
+            var app = builder.Build();
+            if (app.Environment.IsDevelopment() || app.Environment.IsEnvironment("Test"))
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+                using var scope = app.Services.CreateScope();
+                var initialiser = scope.ServiceProvider.GetRequiredService<ApplicationDbContextInitialiser>();
+                await initialiser.InitialiseAsync();
+                await initialiser.SeedAsync();
+            }
+            app.UseHttpsRedirection();
+            app.UseCors("webAppRequests");
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.MapControllers();
+            app.Run();
+        }
+    }
+}
